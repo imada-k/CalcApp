e40b3c5efa2ba4d42336528ab6789c220a0d791a
imada-k <46921280+imada-k@users.noreply.github.com>
2019-10-17 16:01:44 +0900

    initial commit

---
+++ CalcApp/CalcApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcApp
+//
+//  Created by Imada Keitaro on 2019/10/17.
+//  Copyright © 2019 IMADA KEITARO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
